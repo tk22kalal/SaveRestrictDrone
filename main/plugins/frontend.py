d6fa0f0742ecd2ffd1d40808c9c2cd837ff1606d
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2026-05-04 13:09:28 +0530

    Update frontend.py

--- main/plugins/frontend.py
+++ main/plugins/frontend.py
@@ -4,16 +4,11 @@ import time, os
 
 from .. import bot as Drone
 from .. import userbot, Bot
-from .. import FORCESUB as fs
 from main.plugins.pyroplug import get_msg
 from main.plugins.helpers import get_link, join
 
 from telethon import events
 from pyrogram.errors import FloodWait
-
-from ethon.telefunc import force_sub
-
-ft = f"To use this bot you've to join @{fs}."
 
 message = "Send me the message link you want to start saving from, as a reply to this message."
 
@@ -28,10 +23,6 @@ async def clone(event):
         if not link:
             return
     except TypeError:
-        return
-    s, r = await force_sub(event.client, fs, event.sender_id, ft)
-    if s == True:
-        await event.reply(r)
         return
     edit = await event.reply("Processing!")
     try:
